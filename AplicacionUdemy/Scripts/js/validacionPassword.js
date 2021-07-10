5f196c784d37c473a56b6988d0fd4f5a6976b910
Juan Carlos Castro Socla <juan.castro.socla@gmail.com>
2021-07-10 00:34:45 -0500

    Visualización de Contraseña.

---
+++ AplicacionUdemy/Scripts/js/validacionPassword.js
@@ -0,0 +1,31 @@
+﻿/* creación de variables*/
+let password = document.getElementById("txtcontrasena");
+let configpassword = document.getElementById("txtconfirmarcontrasena");
+
+$("#faeye").click(function () {
+    if (password.type === 'password') {
+        password.setAttribute("type", "text");
+        $("#faeye").hide();
+        $("#faeyeslash").show();
+    }
+});
+
+$("#faeyeslash").click(function () {
+    password.setAttribute("type", "password");
+    $("#faeyeslash").hide();
+    $("#faeye").show();
+});
+
+$("#faeyeconfir").click(function () {
+    if (configpassword.type === 'password') {
+        configpassword.setAttribute("type", "text");
+        $("#faeyeconfir").hide();
+        $("#faeyeslashconfir").show();
+    }
+});
+
+$("#faeyeslashconfir").click(function () {
+    configpassword.setAttribute("type", "password");
+    $("#faeyeslashconfir").hide();
+    $("#faeyeconfir").show();
+});
